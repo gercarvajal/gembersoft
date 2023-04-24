ff87983af4d758e61c51c33974cd6ac1a35b4f85
mcgam450 <mcgame450yt@gmail.com>
2023-04-23 22:56:43 -0400

    a

--- prueba3.php
+++ prueba3.php
@@ -5,28 +5,21 @@
     <meta charset="utf-8">
     <title>MJ-Marmol</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
     <!-- Favicon -->
     <link href="resource/images/Iconos/logo.png" rel="icon">
-
     <!-- Google Web Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
-
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <!--linear icon css-->
     <link rel="stylesheet" href="assets/css/linearicons.css">
-
     <!-- Libraries Stylesheet -->
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
     <!-- Customized Bootstrap Stylesheet -->
     <link href="resource/js/css/style.css" rel="stylesheet">
     <audio src="resource/musica/violin.mp3" loop="3" autoplay="true"></audio>
-
 </head>
 
 <style>
@@ -212,23 +205,10 @@
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
-
-
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-
-    <!-- Contact Javascript File -->
-    <script src="mail/jqBootstrapValidation.min.js"></script>
-    <script src="mail/contact.js"></script>
-
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
     <script src="resource/js/css/a.js"></script>
 </body>
 
